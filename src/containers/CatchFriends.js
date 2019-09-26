066809ed5e4d2d7b0d3f6429f531b410cf0d8c3b
melzreal <melissa.real@hotmail.com>
2019-09-26 14:20:01 +0100

    styling cards

--- src/containers/CatchFriends.js
+++ src/containers/CatchFriends.js
@@ -8,16 +8,18 @@ const CatchFriends = (props) => (
  
 	<div> 
 		<h3> Friends To Catch Up with </h3>
-		<div className="Catch">
+		
 		 {props.friends.map(f => 
+		 	<div className="Catch">
 		 	<div>
 			 	<h4> {f.name} </h4>
 			 	<h4> {f.contact} </h4>
 			  <input type="checkbox" checked={props.reached_out} />
 			  <br/>
 		    </div>
+		    </div>
 		)}
-		</div>
+		
 	</div>
 	
 );
